2c30a510402e5aeb3a51a030c832975c7f4ec15f
Lev Lazinskiy <lev@launchdarkly.com>
2019-03-06 20:39:24 -0800

    update server.js to use LaunchDarkly

--- server.js
+++ server.js
@@ -1,46 +1,75 @@
 
 const express = require('express')
 const next = require('next')
-const Featureflow = require('featureflow-node-sdk');
+//const Featureflow = require('featureflow-node-sdk');
+const LaunchDarkly = require('ldclient-node');
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
-const featureflow = new Featureflow.Client({apiKey: 'srv-env-685e066dea464f88be14effbf65cf69c'});
-app.prepare()
-.then(() => {
-  const server = express()
-    server.use((req, res, next) => {
+//const featureflow = new Featureflow.Client({ apiKey: 'srv-env-685e066dea464f88be14effbf65cf69c' });
 
-        let user = new Featureflow.UserBuilder("user1")
-            .withAttribute('country', 'US')
-            .withAttribute('page', 'index')
-            .withAttributes('roles', ['USER_ADMIN', 'BETA_CUSTOMER'])
-            .build();
+app
+  .prepare()
+  .then(() => {
+    const ldclient = LaunchDarkly.init(process.env.LD_SDK_KEY);
+    const server = express()
+    server.use(async (req, res, next) => {
 
-        req.features = featureflow.evaluateAll(user);
-        return next();
+      // let user = new Featureflow.UserBuilder("user1")
+      //   .withAttribute('country', 'US')
+      //   .withAttribute('page', 'index')
+      //   .withAttributes('roles', ['USER_ADMIN', 'BETA_CUSTOMER'])
+      //   .build();
+
+      let user = {
+        "key": 'any',
+        "country": 'AU',
+        "custom": {
+          "page": 'index',
+          "roles": ['USER_ADMIN', 'BETA_CUSTOMER']
+        }
+      }
+
+      allFlags = await ldclient.allFlagsState(user).then(allFlags => {
+        req.features = allFlags.allValues();
+      });
+
+      //req.features = featureflow.evaluateAll(user);
+      return next();
     })
 
     server.get('/api/features', (req, res) => {
-        let user = new Featureflow.UserBuilder("user2")
-            .withAttribute('country', 'US')
-            .withAttribute('page', 'post')
-            .withAttributes('roles', ['USER_ADMIN', 'BETA_CUSTOMER'])
-            .build();
-        res.json(featureflow.evaluateAll(user));
+      // let user = new Featureflow.UserBuilder("user2")
+      //   .withAttribute('country', 'US')
+      //   .withAttribute('page', 'post')
+      //   .withAttributes('roles', ['USER_ADMIN', 'BETA_CUSTOMER'])
+      //   .build();
+      let user = {
+        "key": 'any',
+        "country": 'AU',
+        "custom": {
+          "page": 'index',
+          "roles": ['USER_ADMIN', 'BETA_CUSTOMER']
+        }
+      }
+
+      allFlags = ldclient.allFlagsState(user).then(allFlags => {
+        res.json(allFlags.allValues());
+      })
+      //res.json(featureflow.evaluateAll(user));
     })
 
-  server.get('*', (req, res) => {
-    return handle(req, res)
-  })
+    server.get('*', (req, res) => {
+      return handle(req, res)
+    })
 
-  server.listen(3000, (err) => {
-    if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    server.listen(3000, (err) => {
+      if (err) throw err
+      console.log('> Ready on http://localhost:3000')
+    })
   })
-})
-.catch((ex) => {
-  console.error(ex.stack)
-  process.exit(1)
-})
+  .catch((ex) => {
+    console.error(ex.stack)
+    process.exit(1)
+  })
